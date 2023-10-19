41e1b29ad349fbc97b7f9196f1b503bb630c152d
Eike <eike@eikes-air.lan>
2023-10-18 21:37:05 -0700

    So how are you about to test this? Started to play around with testing of prompt based apps. Injecting a captured response with a 0 temparature turned out to be a reliable way, since we are sticking to a particular model version. Needs more investigation.

--- chatgpt/fun_with_prompts.py
+++ chatgpt/fun_with_prompts.py
@@ -16,25 +16,43 @@ def get_completion(prompt, model="gpt-3.5-turbo",temperature=0):
     )
     return response.choices[0].message["content"]
 
-def prompt_translate_hackernews():
+def prompt_translate_hackernews(hackernews_html=""):
 
-	# we will read the data into a string first:
-	HACKERNEWS_URL = 'https://news.ycombinator.com/'
-	hackernews_data = requests.get(HACKERNEWS_URL).text
+	# For consistent testing, the html will be passed in, in all other cases
+	# we'll need to download it here. In production code you'd want to
+	# cache the response here.
+	if not hackernews_html:
+		HACKERNEWS_URL = 'https://news.ycombinator.com/'
+		hackernews_html = requests.get(HACKERNEWS_URL).text
+		if len(hackernews_html) < 1:
+			print_error(f"""Error downloading hackernews html code. 
+			   Response was: {hackernews_html}
+			   """)
+			return
+
+		## uncomment to update the static file that is being used for unit tests		
+		# with open("tests/hackernews_html_test.txt", "w") as text_file:
+		# 	text_file.write(hackernews_html)
+		
+		print_good("Downloaded hackernews html")
 
 	# we could extract the links with a prompt like this but it turns out to be semi-reliable
 	# prompt = f"""
 	# Extract the links and the coresponding title from inside the <titleline> tags in ```{hackernews_data}```.
 	# Translate the titles to German and renders as a list of links in markdown.
 	# """
-
+	#
+	# Instead we'll do some pre-processing of the data before passing it into the model.
+	# The risk is that if anything in the html changes, we are doomed. 
+	# Not to myself: this is a very interesting trade-off
+	#
 	# the links are in the form of 
 	# <span class="titleline"><a href="{url}" rel="noreferrer">{title}</a>
 	# pattern = '<span class="titleline"><a href="(.*?)" rel="noreferrer">(.*?)</a>'
 	link_pattern = re.compile(r'<span class="titleline"><a href="(.*?)">(.*?)</a>', re.I)
 
     # Find all matches in the HTML content
-	matches = link_pattern.findall(hackernews_data)
+	matches = link_pattern.findall(hackernews_html)
 
     # Iterate through the matches and extract URL and title
 	results = []
@@ -45,8 +63,8 @@ def prompt_translate_hackernews():
 		})
 
 	prompt = f""" Given the input json ```{results}```, extract all the title values.
-	 Translate each of the titles to spanish.
-	 Render the result as a html table.
+	 Translate each of the titles to Spanish and German.
+	 Render the result as a html table with the titles in all three languages.
 	"""
 
 	response = get_completion(prompt)
@@ -57,12 +75,21 @@ def prompt_translate_hackernews():
 def is_api_token_valid():
 	api_key = os.getenv(API_KEY_NAME)
 	if not api_key:
-		print(f"💩 No OpenAI API key found. Set as environment variable like `{API_KEY_NAME}=sk-.....`")
+		print_error(f"No OpenAI API key found. Set as environment variable like `{API_KEY_NAME}=sk-.....`")
 		return False
 		
-	print(f"✅ OpenAI key found as environment variable under `{API_KEY_NAME}`")
+	print_good(f"OpenAI key found as environment variable under `{API_KEY_NAME}`")
 	openai.api_key = api_key
 	return True
+
+def print_error(msg):
+	print(f"💩 {msg}")
+
+def print_good(msg):
+	print(f"✅ {msg}")
+
+def print_info(msg):
+	print(f"⚠️ {msg}")
 
 def main():
 	if is_api_token_valid():
